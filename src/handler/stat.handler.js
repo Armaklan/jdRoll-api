eceae1c42e44096ea1aa4d78edd9e2c7facd1665
Armaklan <lionel.zuber@armaklan.org>
2016-05-08 20:57:09 +0200

    Feat(Stat) - Statistiques ciblés

--- src/handler/stat.handler.js
+++ src/handler/stat.handler.js
@@ -5,10 +5,36 @@ function StatHandler(app, statProvider) {
         });
     });
 
+    app.get('/apiv2/stats/bymonth/:id', function(req, res) {
+        statProvider.byMonthFor(req.params.id).then((rows) => {
+            res.send(rows);
+        });
+    });
+
     app.get('/apiv2/stats/bygame', function(req, res) {
         statProvider.byGame().then((rows) => {
             res.send(rows);
         });
+    });
+
+    app.get('/apiv2/stats/my/byday', function(req, res) {
+        if(!req.phpSession) {
+            return res.send([]);
+        } else {
+            statProvider.byUser(req.phpSession.id).then((rows) => {
+                res.send(rows);
+            });
+        }
+    });
+
+    app.get('/apiv2/stats/my/bygame', function(req, res) {
+        if(!req.phpSession) {
+            return res.send([]);
+        } else {
+            statProvider.byUserAndGame(req.phpSession.id).then((rows) => {
+                res.send(rows);
+            });
+        }
     });
 }
 
--- src/provider/stat.provider.js
+++ src/provider/stat.provider.js
@@ -8,6 +8,22 @@ function StatProvider(connection) {
             GROUP BY dat;`);
     };
 
+    this.byMonthFor = function(campagneId) {
+        return connection.query(`SELECT
+            DATE_FORMAT(create_date, '%Y,%m,01') as dat,
+            count(*) as cpt
+            FROM posts
+            LEFT JOIN topics
+                ON topics.id = posts.topic_id
+            LEFT JOIN sections
+                ON sections.id = topics.section_id
+            LEFT JOIN campagne
+                ON campagne.id = sections.campagne_id
+            WHERE posts.user_id IS NOT NULL
+            AND campagne.id = ?
+            GROUP BY dat;`, [campagneId]);
+    };
+
     this.byGame = function() {
         return connection.query(`SELECT
             campagne.name as game,
@@ -23,6 +39,30 @@ function StatProvider(connection) {
             AND campagne.statut = 0
             OR campagne.statut IS NULL
             GROUP BY game;`);
+    };
+
+    this.byUserAndGame = function(userId) {
+        return connection.query(`SELECT
+            campagne.name as game,
+            count(*) as cpt
+            FROM posts
+            LEFT JOIN topics
+                ON topics.id = posts.topic_id
+            LEFT JOIN sections
+                ON sections.id = topics.section_id
+            LEFT JOIN campagne
+                ON campagne.id = sections.campagne_id
+            WHERE posts.user_id = ?
+            GROUP BY game;`, [userId]);
+    };
+
+    this.byUser = function(userId) {
+        return connection.query(`SELECT
+            DATE_FORMAT(create_date, '%Y-%m-%d') as dat,
+            count(*) as cpt
+            FROM posts
+            WHERE user_id = ?
+            GROUP BY dat`, [userId]);
     };
 }
 
